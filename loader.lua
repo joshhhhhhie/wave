f9e0b815c63f146dce1a148746c8e450081af65c
wave <joshwessels13@gmail.com>
2026-03-26 18:42:21 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+getgenv().syscure_settings = {
+    syscure_loader = [[loadstring(game:HttpGet("https://auth.syscure.vip/obf/0b3a4d156c75712a3b8576a454ae4737.lua"))()]], 
+    get_key_link = "https://syscure.vip/get-key/b9cf32ce7e6048602f8e5c14",
+    save_key = true
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/joshhhhhhie/wave/refs/heads/main/key.lua"))()
